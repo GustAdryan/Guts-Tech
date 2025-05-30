87b220796ca8fab1354a75b9340d926f9c2a88ea
GustAdryan <136760762+GustAdryan@users.noreply.github.com>
2025-05-30 18:53:34 -0300

    Delete src directory

--- src/components/menu-togge.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("menu-toggle");
-  const navList = document.getElementById("menu-list");
-
-  toggle.addEventListener("click", () => {
-    navList.classList.toggle("-translate-x-full");
-    navList.classList.toggle("translate-x-0");
-  });
-});
--- src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-import UI from "./ui.js";
-import Kart from "./kart.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  UI.renderizarProdutos();
-  Kart.init();
-});
